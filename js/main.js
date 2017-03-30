f88b7f21772599bf4dd58f6f5f9ce6b71ef02841
RhondaHoonjan <rhonda.hoonjan@gmail.com>
2017-03-30 15:57:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,51 +1,51 @@
-function askQuestions() {
+//function askQuestions() {
 
-	var firstName = prompt('What is your first name?').trim();
-	var lastName = prompt('What is your last name?').trim();
-	var fullName = firstName + '' + lastName;
+	//var firstName = prompt('What is your first name?').trim();
+	//var lastName = prompt('What is your last name?').trim();
+	//var fullName = firstName + '' + lastName;
 
-	console.log(fullName);
+	//console.log(fullName);
 	// above is just a diagnostic message
 
-	var age = prompt('How old are you?');
-	age = ParseInt(age);
+	//var age = prompt('How old are you?');
+	//age = ParseInt(age);
 
-	if (age >= 18) {
+	//if (age >= 18) {
 
-		console.log('User is an adult');
+		//console.log('User is an adult');
 
-	} else if (age >= 13) {
+	//} else if (age >= 13) {
 
-		console.log('User is a teenager');
+		//console.log('User is a teenager');
 
-	} else {
+	//} else {
 
-		console.log('User is a child');
+		//console.log('User is a child');
 
-	}
+	//}
 
-	if (firstName.toLowerCase().trim() === 'general' && lastName.toLowerCase().trim !== 'assembly') {
+	//if (firstName.toLowerCase().trim() === 'general' && lastName.toLowerCase().trim !== 'assembly') {
 
-		console.log('Greetings, General!');
-	}	
+		//console.log('Greetings, General!');
+	//}	
 
-}	
+//}	
 	
 
 
 
 
 //When the page has loaded
-$(function () {
+//$(function () {
 
-		$('img').on('click', askQuestions);
+		//$('img').on('click', askQuestions);
 
 	//When the user clicks an h3
-	$('h3').on ('click', function (){
+	//$('h3').on ('click', function (){
 
 		//Toggle the next element
-		$(this).next().slideToggle(50);
+		//$(this).next().slideToggle(50);
 
-	});
+	//});
 
-});
+//});
